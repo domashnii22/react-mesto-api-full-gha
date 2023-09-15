5bd6c34199dd6a068adb0b7e9689cb083a70ac68
Aleksey Guskov <domashnii.al@yandex.ru>
2023-09-15 14:20:24 +0300

    изменение IP

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const auth = require("./middlewares/auth");
 const errorHandler = require("./middlewares/error-handler");
 const NotFoundError = require("./errors/NotFoundError");
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/testdb" } =
+const { PORT = 3000, DB_URL = "mongodb://158.160.122.51:27017/testdb" } =
   process.env;
 
 const app = express();
